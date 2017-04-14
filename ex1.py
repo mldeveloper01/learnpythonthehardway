c38e2ef0ec63d6bedb41191de57fc78d4f80b929
eesah <kksharapov@gmail.com>
2017-04-14 17:03:46 +0500

    a

---
+++ ex1.py
@@ -0,0 +1,10 @@
+print "Hello World!"
+print "Hello Again"
+print "I like typing this."
+print "This is fun"
+print 'Yay! Printing.'
+print "I'd much rather you 'not'."
+print 'I "said" do not touch this.'
+print "Another line of code"
+print "Some other code"
+#some comment to test it out
---
+++ ex3.py
@@ -0,0 +1,23 @@
+print "I will now count my chickens:"
+
+print "Hens", 25.00 + 30.00 / 6.00 #prints put hens number
+print "Roosters", 100.00 - 25.00 * 3.00 % 4.00
+
+print "Now I will count the eggs:"
+
+print 3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6
+
+print "Is it true that 3 + 2 < 5 - 7?"
+
+print 3.00 + 2.00 < 5.00 - 7.00
+
+print "What is 3 + 2?", 3 + 2
+print "What is 5 - 7?", 5 - 7
+
+print "Oh, that's why it's False."
+
+print "How about some more."
+
+print "Is it greater?", 5 > -2
+print "Is it greater or equal?", 5 >= -2
+print "Is it less or equal?", 5 <= -2
